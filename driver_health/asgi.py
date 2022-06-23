a77052fad2d988fb0f546ee7d75e0990124b27e7
Ayanda <ayatech.projects@gmail.com>
2022-06-23 22:36:11 +0200

    changes

--- driver_health/asgi.py
+++ driver_health/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'driver_health.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'driver_health.settings.production')
 
 application = get_asgi_application()
