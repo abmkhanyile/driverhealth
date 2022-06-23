5edeb926e96894ad8196225add61437eb2f5706b
Ayanda <ayatech.projects@gmail.com>
2022-06-23 10:08:25 +0200

    first push

---
+++ home/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.generic.base import View, ContextMixin
+
+# displays the homepage.
+class Home(ContextMixin, View):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get(self, request, **kwargs):
+        return render(request, self.template_name, self.get_context_data())
