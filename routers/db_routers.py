5de5ae04995e3987b4185592ba11c59a5e2b149b
Ayanda <ayatech.projects@gmail.com>
2022-07-10 19:49:35 +0200

    changes

--- routers/db_routers.py
+++ routers/db_routers.py
@@ -10,3 +10,11 @@ class AuthRouter:
         if model._meta.app_label in self.route_app_labels:
             return 'auth_db'
         return None
+
+    def db_for_write(self, model, **hints):
+        """
+        Attempts to write auth and contenttypes models go to auth_db.
+        """
+        if model._meta.app_label in self.route_app_labels:
+            return 'auth_db'
+        return None
