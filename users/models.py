fbaf3c5b0d8d55ccbbfd4d39999aea0df71bbe95
Ayanda <ayatech.projects@gmail.com>
2022-07-10 20:33:08 +0200

    changes

--- users/models.py
+++ users/models.py
@@ -2,7 +2,17 @@ from django.db import models
 
 # Create your models here.
 class CustomUser(models.Model):
-    pass
+    username = models.CharField()
+    password = models.CharField()
+    first_name =models.CharField()
+    last_name =models.CharField()
+    email = models.EmailField()
+    contactNumber = models.CharField()
+    driverhealth_id = models.CharField()
+    terms = models.BooleanField()
+    is_superuser = models.BooleanField()
+    is_active = models.BooleanField()
+
 
 class DriverProfile(models.Model):
     pass
