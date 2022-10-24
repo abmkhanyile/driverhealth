ff5446b694fdc0892dfda250e2a8efa21e6e00be
Ayanda <ayatech.co@gmail.com>
2022-10-24 20:13:55 +0200

    changes

--- driver_health/celery.py
+++ driver_health/celery.py
@@ -3,10 +3,10 @@ import os
 from celery import Celery
 from django.conf import settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'driver_health.settings.base_settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'driver_health.settings.production')
 
 app = Celery('driver_health', backend='redis://localhost', broker=settings.CELERY_BROKER_URL)
 
-app.config_from_object('django.conf:settings', namespace='CELERY')
+# app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
