d37627d17eade18a614e03268b419b910707be82
Ayanda <ayatech.co@gmail.com>
2022-10-26 09:31:27 +0200

    changes

--- companies/tasks.py
+++ companies/tasks.py
@@ -1,17 +1,16 @@
 from __future__ import absolute_import, unicode_literals
-# from companies.models import Company
+from companies.models import Company
 
 from celery import shared_task
 
 
 
 @shared_task
-def update_limits(x, y):
-    return x+y
-    # companies = Company.objects.all()
-    # for company in companies:
-    #     if company.driver_requests_limit != 10:
-    #         company.driver_requests_limit = 10
-    #         company.save()
+def update_limits():
+    companies = Company.objects.all()
+    for company in companies:
+        if company.driver_requests_limit != 10:
+            company.driver_requests_limit = 10
+            company.save()
         
         
